017eef31f2eb31d3425caafef34f942eeaa470d5
Himel <hasibulislam8381@gmail.com>
2022-08-01 18:14:38 +0600

    repeated character

---
+++ repeated1.php
@@ -0,0 +1,14 @@
+<?php
+$data = 'abhueygggggwwpppwi1233333333884';
+$str = trim($data,"0123456789");
+// split the string per character and count the number of occurrences like a=1,b=1,w=2
+$totals = array_count_values( str_split($str) );
+
+// sort the totals so that the most frequent letter is first
+arsort( $totals );
+
+// show which letter occurred the most frequently
+echo array_keys( $totals )[0] ;
+
+
+?>
