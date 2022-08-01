0dc3b7ae1d8ab177265a2ffcebde3e128a954bed
Hasibul Islam Himel <63547094+Hasibulislam8381@users.noreply.github.com>
2022-08-01 18:16:26 +0600

    Comment adde

--- repeated1.php
+++ repeated1.php
@@ -1,5 +1,6 @@
 <?php
 $data = 'abhueygggggwwpppwi1233333333884';
+//We will trim the integr from an string
 $str = trim($data,"0123456789");
 // split the string per character and count the number of occurrences like a=1,b=1,w=2
 $totals = array_count_values( str_split($str) );
